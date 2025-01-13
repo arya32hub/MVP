47bfe831ff97e0e84629a18942be35cb1b2ba9a0
Sohail Shrestha <sohailshrestha2@gmail.com>
2025-01-13 17:32:22 +0545

    color palatte added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,20 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "main-blue": "#00236C",
+      "mountain-lake": "#87AEEE",
+      tornado: "#798999",
+      smoke: "#F4F4F4F4",
+      "gray-900": "#333335",
+      "gray-500": "#A0AEBC",
+      "gray-400": "#BEBEBE",
+      "gray-200": "#DDE0E3F4",
+      "ice-cube": "#F6F9FF",
+      "ice-cube-100": "#F1F7FF",
+      red: "#E43451",
+      success: "#00F386",
+    },
     extend: {
       colors: {
         background: "var(--background)",
